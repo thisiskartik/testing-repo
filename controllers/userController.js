feb9ad98ac58dc1afd70d9fc6fd53d2d1d877715
Kartik Arora <kartikcsengineer@gmail.com>
2024-06-15 11:31:28 +0530

    Initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,111 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+
+const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPassword(password))) {
+		generateToken(res, user._id);
+
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			role: user.role,
+		});
+	} else {
+		res.status(401);
+		throw new Error("Invalid email or password");
+	}
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password, role } = req.body;
+
+	const userExists = await User.findOne({ email });
+
+	if (userExists) {
+		res.status(400);
+		throw new Error("User already exists");
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+		role,
+		currentStatus: role === "student" ? "looking_for_opportunities" : undefined,
+	});
+
+	if (user) {
+		generateToken(res, user._id);
+
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			role: user.role,
+			currentStatus: user.currentStatus,
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid user data");
+	}
+});
+
+const logoutUser = (req, res) => {
+	res.cookie("jwt", "", {
+		httpOnly: true,
+		expires: new Date(0),
+	});
+	res.status(200).json({ message: "Logged out successfully" });
+};
+
+const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			role: user.role,
+			currentStatus: user.currentStatus,
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+		user.role = req.body.role || user.role;
+		user.currentStatus = req.body.currentStatus || user.currentStatus;
+
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+
+		const updatedUser = await user.save();
+
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			role: updatedUser.role,
+			currentStatus: updatedUser.currentStatus,
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
+});
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile };
---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			enum: ["student", "mentor", "admin", "teacher", "volunteer"],
+			required: true,
+		},
+		currentStatus: {
+			type: String,
+			enum: ["looking_for_opportunities", "applied", "offer_received", "na"],
+			default: "na",
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) {
+		next();
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
