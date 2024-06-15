7134692c9f2fb9b6268c32f499b40b49313f3fe8
Kartik Arora <kartikcsengineer@gmail.com>
2024-06-15 15:22:41 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const port = process.env.PORT || 5000;
@@ -12,6 +11,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
